7a78994a148d1d792b9fa4a16bfe4c0be923ba33
leonardo <leonardosano1995@gmail.com>
2020-03-31 00:05:47 +0200

    progetto completato

--- lib/story.dart
+++ lib/story.dart
@@ -1,26 +1,18 @@
 class Story {
 
   String _storyTitle;
-  String _choiche1;
-  String _choiche2;
+  String _choice1;
+  String _choice2;
 
   Story({String storyTitle,String choice1,String choice2}){
     this._storyTitle = storyTitle;
-    this._choiche1 = choice1;
-    this._choiche2 = choice2;
+    this._choice1 = choice1;
+    this._choice2 = choice2;
   }
 
-  String get choiche2 => this._choiche2;
+  String get choice2 => this._choice2;
 
-  set choiche2(String value) {
-    this._choiche2 = value;
-  }
-
-  String get choiche1 => this._choiche1;
-
-  set choiche1(String value) {
-    this._choiche1 = value;
-  }
+  String get choice1 => this._choice1;
 
   String get storyTitle => this._storyTitle;
 
