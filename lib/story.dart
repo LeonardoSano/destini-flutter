5ba8e55cde70b1873324ffd3dfbba979b16e7fd1
leonardo <leonardosano1995@gmail.com>
2020-03-30 23:25:47 +0200

    costruite classi e metodi principali

--- lib/story.dart
+++ lib/story.dart
@@ -1,5 +1,32 @@
-//TODO: Step 2 - Create a new class called Story.
+class Story {
 
-//TODO: Step 3 - Create 3 properties for this class, A. storyTitle to store the story text. B. choice1 to store the text for choice 1, C. choice2 to store the text for choice 2.
+  String _storyTitle;
+  String _choiche1;
+  String _choiche2;
 
-//TODO: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
+  Story({String storyTitle,String choice1,String choice2}){
+    this._storyTitle = storyTitle;
+    this._choiche1 = choice1;
+    this._choiche2 = choice2;
+  }
+
+  String get choiche2 => this._choiche2;
+
+  set choiche2(String value) {
+    this._choiche2 = value;
+  }
+
+  String get choiche1 => this._choiche1;
+
+  set choiche1(String value) {
+    this._choiche1 = value;
+  }
+
+  String get storyTitle => this._storyTitle;
+
+  set storyTitle(String value) {
+    this._storyTitle = value;
+  }
+
+
+}
